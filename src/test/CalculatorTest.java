4467ba04077d033a53022d8349330ed0daa84882
toshiba <toshiba@toshiba-PC>
2020-03-01 20:17:30 +0200

    First Commit

---
+++ src/test/CalculatorTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void test() {
+		assertEquals("Error in Addition operation()",3,java.Calculator.add(1, 2));
+		assertEquals("Error in Addition operation()",-3,java.Calculator.add(-1, -2));
+		assertEquals("Error in Addition operation()",9,java.Calculator.add(9, 0));
+	
+	}
+
+}
